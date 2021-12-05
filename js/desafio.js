a3cc9f6de3b063700a4186e0b4d921af0d52cba2
Lester <lester@magiana.com.ar>
2021-12-05 16:37:41 -0300

    desafio

---
+++ js/desafio.js
@@ -0,0 +1,29 @@
+// ciclo y function
+
+// while(metodoPago !==''){
+//     switch (metodoPago){
+//         case 'tarjeta':
+//             alert("Pagaste con tarjeta");
+//             break;
+//         case 'transferencia':
+//             alert('Pagaste con transferencia');
+//             break;
+//         case 'null':
+//             alert('Esto no es un metodo de pago')
+//             break;
+//         default:
+//             alert('No agregaste metodo de pago');
+//             break;
+//     }
+
+//     metodoPago = prompt('Ingresa un metodo de pago')
+// }
+
+
+
+// function conversor() {
+//     monto = +prompt("Ingrese el monto")
+//       alert ('La cantidad de dolares es:' + monto / 200 )
+// } 
+  
+//   conversor();
