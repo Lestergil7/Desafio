1a2c6fa6fb262019cf66bdedd33368411a649df6
Lester <lester@magiana.com.ar>
2022-01-29 01:52:34 -0300

    listo

--- js/index.js
+++ js/index.js
@@ -189,3 +189,47 @@ function clearTask(e) {
 function clearTaskFromLocalStorage() {
     localStorage.clear();
 }
+
+
+// FetchAPI
+
+  
+  const card = document.getElementById('carta')
+  const city = document.getElementById('city');
+  const tempImg = document.getElementById('temp-img');
+  const temp = document.getElementById('temp');
+  const weather = document.getElementById('weather');
+  const range = document.getElementById('range');
+  
+  function updateImages(data) {
+    const temp = toCelsius(data.main.temp);
+    let src = 'images/temp-mid.png';
+    if (temp > 26) {
+      src = 'images/temp-high.png';
+    } else if (temp < 20) {
+      src = 'images/temp-low.png';
+    }
+    tempImg.src = src;
+  }
+
+  
+ 
+  function toCelsius(kelvin) {
+    return Math.round(kelvin - 273.15);
+  }
+  
+
+let url = 'https://api.openweathermap.org/data/2.5/weather?id=3435907&APPID=b37458221d63f82a358ec6013726ae99'
+
+fetch(url)
+   .then(response => response.json())
+   .then(data => mostrarData(data))
+   .catch(error => console.log(error))
+
+  const mostrarData = (data) =>{
+    city.innerHTML = `${data.name}, ${data.sys.country}`;
+    temp.innerHTML = `${toCelsius(data.main.temp)}c`;
+    weather.innerHTML = data.weather[0].description;
+    range.innerHTML = `${toCelsius(data.main.temp_min)}c / ${toCelsius(data.main.temp_max)}c`;
+    updateImages(data);
+   }
